f7e486ca9f79308297086e36197a9e033edabdc2
Ryan Casler <ryancasler@Ryans-MacBook-Pro.local>
2015-12-21 20:35:26 -0500

    push

--- app/src/main/java/com/example/spartahack/spartahack2016/MyApplication.java
+++ app/src/main/java/com/example/spartahack/spartahack2016/MyApplication.java
@@ -3,16 +3,15 @@ package com.example.spartahack.spartahack2016;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseInstallation;
 
 /**
  * Created by ryan on 11/3/15.
  */
-public class MyApplication extends Application
-{
+public class MyApplication extends Application {
     @Override
     public void onCreate() {
-        super.onCreate();
-
         Parse.initialize(this, Keys.PARSE_1, Keys.PARSE_2);
+        ParseInstallation.getCurrentInstallation().saveInBackground();
     }
 }
