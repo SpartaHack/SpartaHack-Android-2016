919ce6ffd524b6dff192b5c2f97814712d029a8f
Ryan Casler <ryancasler@ryans-mbp.attlocal.net>
2015-12-23 21:28:31 -0500

    Add display notifications in the notifications tab

---
+++ app/src/main/java/com/example/spartahack/spartahack2016/Adapters/NotificationAdapter.java
@@ -0,0 +1,90 @@
+package com.example.spartahack.spartahack2016.Adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.spartahack.spartahack2016.Activity.MainActivity;
+import com.example.spartahack.spartahack2016.Model.PushNotification;
+import com.example.spartahack.spartahack2016.R;
+
+import java.util.List;
+
+/**
+ * Created by ryancasler on 12/23/15.
+ *
+ * Adapter for notifications displayed in the notificaiton fragment.
+ *
+ *
+ */
+public class NotificationAdapter extends BaseAdapter {
+
+    /**
+     * Reference to the main activity for context to get images as well as get the layout inflater
+     */
+    private MainActivity context;
+
+    /**
+     * The list of notifications that is being displayed
+     */
+    private List<PushNotification> notifications;
+
+    public NotificationAdapter(MainActivity context, @SuppressWarnings("SameParameterValue") int resource, List<PushNotification> notifications) {
+        this.context = context;
+        this.notifications = notifications;
+    }
+
+    @Override
+    public int getCount() {
+        return notifications.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return notifications.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        NotificationViewHolder holder;
+
+        if (convertView == null) {
+
+            LayoutInflater inflater = context.getLayoutInflater();
+            convertView = inflater.inflate(R.layout.layout_notificaiton_item, parent, false);
+            holder = new NotificationViewHolder();
+
+            holder.message = (TextView) convertView.findViewById(R.id.message);
+            holder.title = (TextView) convertView.findViewById(R.id.title);
+
+            convertView.setTag(holder);
+
+        } else {
+            holder = (NotificationViewHolder) convertView.getTag();
+        }
+
+        final PushNotification pushNotification = notifications.get(position);
+
+        holder.title.setText(pushNotification.getTitle());
+        holder.message.setText(pushNotification.getMessage());
+
+        return convertView;
+    }
+
+    /**
+     * Class for the Viewholder pattern
+     */
+    static class NotificationViewHolder {
+        TextView title;
+        TextView message;
+    }
+
+}
--- app/src/main/java/com/example/spartahack/spartahack2016/Fragment/NotificationFragment.java
+++ app/src/main/java/com/example/spartahack/spartahack2016/Fragment/NotificationFragment.java
@@ -1,21 +1,35 @@
 package com.example.spartahack.spartahack2016.Fragment;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.example.spartahack.spartahack2016.Activity.MainActivity;
+import com.example.spartahack.spartahack2016.Adapters.NotificationAdapter;
+import com.example.spartahack.spartahack2016.Model.PushNotification;
 import com.example.spartahack.spartahack2016.R;
 
+import java.util.List;
+
+import butterknife.Bind;
 import butterknife.ButterKnife;
+import io.realm.Realm;
+import io.realm.RealmResults;
+import io.realm.Sort;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class NotificationFragment extends BaseFragment {
+public class NotificationFragment extends BaseFragment implements SwipeRefreshLayout.OnRefreshListener {
+
+    @Bind(R.id.notificaiton_list) ListView notificationList;
+    @Bind(R.id.swipe_refresh) SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -24,7 +38,31 @@ public class NotificationFragment extends BaseFragment {
 
         ButterKnife.bind(this, view);
 
+        updateNotifications();
+
+        swipeRefreshLayout.setOnRefreshListener(this);
+        swipeRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_bright, android.R.color.holo_green_light);
+
         return view;
+    }
+
+    private void updateNotifications(){
+        Realm realm = Realm.getDefaultInstance();
+        RealmResults<PushNotification> results = realm.where(PushNotification.class).findAllSorted("pinned", Sort.DESCENDING);
+        List<PushNotification> notifications = results.subList(0, results.size());
+
+        NotificationAdapter adapter = new NotificationAdapter((MainActivity) getActivity(), R.layout.layout_notificaiton_item, notifications);
+        notificationList.setAdapter(adapter);
+    }
+
+    @Override
+    public void onRefresh() {
+        refresh(true);
+    }
+
+    private void refresh(boolean force){
+        updateNotifications();
+        swipeRefreshLayout.setRefreshing(false);
     }
 
 
