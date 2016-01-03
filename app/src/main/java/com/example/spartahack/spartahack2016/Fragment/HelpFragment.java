5396796ddfbfe6a82bbd512628729a67fa7e8578
Aasir <aasiruddinw@gmail.com>
2016-01-02 19:03:23 -0500

    Complete except User Logged In check

--- app/src/main/java/com/example/spartahack/spartahack2016/Fragment/HelpFragment.java
+++ app/src/main/java/com/example/spartahack/spartahack2016/Fragment/HelpFragment.java
@@ -3,8 +3,7 @@ package com.example.spartahack.spartahack2016.Fragment;
 
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,10 +15,14 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.spartahack.spartahack2016.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -36,46 +39,52 @@ public class HelpFragment extends BaseFragment {
     @Bind(R.id.descLayout) TextInputLayout inputLayoutDesc;
 
     //String spinnerText;
-    final ParseUser user = ParseUser.getCurrentUser();
+    ParseUser user;
+    List<ParseObject> categoryList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_help, container, false);
+        View view;
+        user = ParseUser.getCurrentUser();
+        if (user == null){
+            Log.d("NOT LOGGED IN", "SHIT IS NOT BROKEN YO");
+            return inflater.inflate(R.layout.layout_notloggedin, container, false);
+        }
+        else {
+            Log.d("NOT LOGGED IN", "SHIT IS BROKEN YO");
+            view = inflater.inflate(R.layout.fragment_help, container, false);
 
-        ButterKnife.bind(this, view);
+            ButterKnife.bind(this, view);
 
-        // Category Spinner
-        ArrayList<String> categoryArray = new ArrayList<String>();
-        categoryArray.add("iOS");
-        categoryArray.add("Android");
-        categoryArray.add("Backend");
-        categoryArray.add("Frontend");
-        categoryArray.add("Windows");
+            // Category Spinner
+            final ArrayList<String> categoryArray = new ArrayList<String>();
+            ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("HelpDesk");
+            query.findInBackground(new FindCallback<ParseObject>() {
+                public void done(List<ParseObject> markers, ParseException e) {
+                    if (e == null) {
+                        categoryList = markers;
+                        for (ParseObject object : markers) {
+                            categoryArray.add(object.get("category").toString());
+                        }
+                        ArrayAdapter<String> spinnerCategoryArrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, categoryArray);
+                        categorySpinner.setAdapter(spinnerCategoryArrayAdapter);
+                    } else {
+                        // handle Parse Exception here
+                    }
+                }
+            });
 
-        ArrayAdapter<String> spinnerCategoryArrayAdapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_dropdown_item, categoryArray);
-        categorySpinner.setAdapter(spinnerCategoryArrayAdapter);
+            // Room Spinner
+            ArrayAdapter<String> spinnerRoomArrayAdapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.roomArray));
+            roomSpinner.setAdapter(spinnerRoomArrayAdapter);
 
-        // Room Spinner
-        ArrayList<String> roomArray = new ArrayList<String>();
-        roomArray.add("Cedar Village");
-        roomArray.add("Chandler Crossings");
-        roomArray.add("University Villa");
-        roomArray.add("University Village");
-        roomArray.add("2900 Apartments");
-
-        ArrayAdapter<String> spinnerRoomArrayAdapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_dropdown_item, roomArray);
-        roomSpinner.setAdapter(spinnerRoomArrayAdapter);
-
-        //name.addTextChangedListener(new HelpTextWatcher(name));
-        //description.addTextChangedListener(new HelpTextWatcher(description));
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                submitForm();
-            }
-        });
-
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    submitForm();
+                }
+            });
+        }
 
         return view;
     }
@@ -85,14 +94,23 @@ public class HelpFragment extends BaseFragment {
             return;
         }
 
-        ParseObject data = new ParseObject("Concierge");
-        if (user != null) {
-            data.put("userId", user.getObjectId().toString());
-        }
-        data.put("name", name.getText().toString());
-        data.put("room", roomSpinner.getSelectedItem().toString());
+        ParseObject data = new ParseObject("HelpDeskTickets");
+//        if (user != null) {
+//            data.put("userId", user.getObjectId().toString());
+//        }
+        //data.put("name", name.getText().toString());
+        //data.put("room", roomSpinner.getSelectedItem().toString());
         data.put("description", description.getText().toString());
-        data.put("category", categorySpinner.getSelectedItem().toString());
+
+        //Category Relation Object
+        ParseObject categoryObject = null;
+        for (ParseObject object : categoryList){
+            if (object.get("category") == categorySpinner.getSelectedItem().toString()){
+                categoryObject = object;
+            }
+        }
+
+        data.put("category", categoryObject);
         data.saveInBackground();
         Toast.makeText(getActivity().getBaseContext(), "Submitted Successfully", Toast.LENGTH_SHORT).show();
         name.setText("");
@@ -127,32 +145,6 @@ public class HelpFragment extends BaseFragment {
     private void requestFocus(View view) {
         if (view.requestFocus()) {
             getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-        }
-    }
-
-    private class HelpTextWatcher implements TextWatcher {
-
-        private View view;
-
-        private HelpTextWatcher(View view) {
-            this.view = view;
-        }
-
-        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-        }
-
-        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-        }
-
-        public void afterTextChanged(Editable editable) {
-            switch (view.getId()) {
-                case R.id.name:
-                    validateName();
-                    break;
-                case R.id.description:
-                    validateDesc();
-                    break;
-            }
         }
     }
 
