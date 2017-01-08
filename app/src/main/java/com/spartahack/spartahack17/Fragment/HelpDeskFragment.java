5d7ee17276b614106070d3d96636deb529c0816b
Ryan Casler <caslerry@msu.edu>
2017-01-08 13:02:33 -0500

    add names, finish submitting tickets

--- app/src/main/java/com/spartahack/spartahack17/Fragment/HelpDeskFragment.java
+++ app/src/main/java/com/spartahack/spartahack17/Fragment/HelpDeskFragment.java
@@ -1,12 +1,15 @@
 package com.spartahack.spartahack17.Fragment;
 
+import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 
+import com.spartahack.spartahack17.Activity.MainActivity;
 import com.spartahack.spartahack17.Cache;
 import com.spartahack.spartahack17.R;
 import com.spartahack.spartahack17.Retrofit.GSONMock;
@@ -39,27 +42,34 @@ public class HelpDeskFragment extends BaseFragment {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), simple_spinner_item, arraySpinner);
         cateogrySpinner.setAdapter(adapter);
 
-        locationText.setText(null);
-        descriptionText.setText(null);
+        resetForm();
+        setContent();
     }
 
     @Override int getLayout() { return R.layout.fragment_help_desk; }
 
+    /**
+     * Submit a help desk ticket
+     */
     @OnClick(R.id.button_submit) void  onSubmit() {
+
+        // verify the user set a location
         if (TextUtils.isEmpty(locationText.getText().toString())){
             locationText.setError("Must set a location!");
             return;
         }
 
+        // verify the user set a description
         if (TextUtils.isEmpty(descriptionText.getText().toString())){
             descriptionText.setError("Must set a description!");
             return;
         }
 
+        // create the ticket
         GSONMock.CreateMentorshipTicketRequest ticket = new GSONMock.CreateMentorshipTicketRequest(
                 "#" + cateogrySpinner.getSelectedItem().toString(),
-                String.valueOf(Cache.INSTANCE.getSession().getId()),
-                "Location: " + locationText.getText().toString() + ", Description: " + descriptionText.getText().toString());
+                "(" +  locationText.getText().toString() + ") " + Cache.INSTANCE.getSession().getFullName(),
+                descriptionText.getText().toString());
 
         SlackAPIService.INSTANCE.getRestAdapter()
                 .addTicket(ticket)
@@ -76,6 +86,7 @@ public class HelpDeskFragment extends BaseFragment {
 
                     if (res != null && res.equals("ok")) {
                         Snackbar.make(signedIn, "Ticket Created", Snackbar.LENGTH_SHORT).show();
+                        resetForm();
                     } else {
                         Snackbar.make(signedIn, "Error Creating Ticket", Snackbar.LENGTH_SHORT).show();
                     }
@@ -83,12 +94,38 @@ public class HelpDeskFragment extends BaseFragment {
                 }, throwable -> Snackbar.make(signedIn, "Error Creating Ticket", Snackbar.LENGTH_SHORT).show());
     }
 
-//    @OnClick(R.id.login) void onLogin() {
-//        MainActivity activity = ((MainActivity) getActivity());
-//        ProfileFragment fragment = new ProfileFragment();
-//        Bundle bundle = new Bundle();
-//        bundle.putString(ProfileFragment.I_EXTRA_FROM, ProfileFragment.I_EXTRA_FROM);
-//        fragment.setArguments(bundle);
-//        activity.switchContent(R.id.container, fragment);
-//    }
+    /**
+     * Redirect the user to the login screen
+     */
+    @OnClick(R.id.button_login) void onLogin() {
+        MainActivity activity = ((MainActivity) getActivity());
+        ProfileFragment fragment = new ProfileFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString(ProfileFragment.I_EXTRA_FROM, ProfileFragment.I_EXTRA_FROM);
+        fragment.setArguments(bundle);
+        activity.switchContent(R.id.container, fragment);
+    }
+
+    /**
+     * Pick what content should be shown
+     */
+    private void setContent() {
+        if (Cache.INSTANCE.hasSession()){
+            signedOut.setVisibility(View.GONE);
+            signedIn.setVisibility(View.VISIBLE);
+        } else {
+            signedIn.setVisibility(View.GONE);
+            signedOut.setVisibility(View.VISIBLE);
+        }
+    }
+
+    /**
+     * Reset all values in the form
+     */
+    private void resetForm() {
+        locationText.setText(null);
+        descriptionText.setText(null);
+        cateogrySpinner.setSelection(0);
+    }
+
 }
