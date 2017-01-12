9d38b6eddc0b154b345e3cd211ebfccf1aa4739a
memuyskens <morganmuyskens@gmail.com>
2017-01-11 19:42:45 -0500

    Added a base64 decoder for the sponsor PNGs.

--- app/src/main/java/com/spartahack/spartahack17/Adapters/CompanyListAdapter.java
+++ app/src/main/java/com/spartahack/spartahack17/Adapters/CompanyListAdapter.java
@@ -1,6 +1,7 @@
 package com.spartahack.spartahack17.Adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 import com.spartahack.spartahack17.Model.Company;
 import com.spartahack.spartahack17.R;
+import com.spartahack.spartahack17.Utils.Base64Decoder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -50,8 +52,7 @@ public class CompanyListAdapter extends RecyclerView.Adapter<CompanyListAdapter.
 
     public CompanyListAdapter(Context context, ArrayList<Company> data) {
         mContext = context;
-        if (data != null)
-            mData = data;
+        if (data != null) mData = data;
         else mData = new ArrayList<>();
     }
 
@@ -65,9 +66,9 @@ public class CompanyListAdapter extends RecyclerView.Adapter<CompanyListAdapter.
     @Override public void onBindViewHolder(SimpleViewHolder holder, final int position) {
         final Company c = mData.get(position);
 
-        if (c.getLogo_png_light()!= null){
-            // TODO: 1/6/17 fix this loading right 
-            Glide.with(mContext).load(c.getLogo_png_light()).into(holder.logo);
+        if (c.getLogo_png_light() != null){
+            Bitmap bitmap = Base64Decoder.decodeSampledBitmapFromBase64(c.getLogo_png_light(), 150, 150);
+            holder.logo.setImageBitmap(bitmap);
         } else {
             Glide.with(mContext).load(R.drawable.logo_17).into(holder.logo);
         }
---
+++ app/src/main/java/com/spartahack/spartahack17/Utils/Base64Decoder.java
@@ -0,0 +1,55 @@
+package com.spartahack.spartahack17.Utils;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+
+/**
+ * Created by memuyskens on 1/11/17.
+ * SpartaHack-Android
+ */
+
+public class Base64Decoder {
+
+    public static Bitmap decodeSampledBitmapFromBase64(String base64String,
+                                                        int reqWidth, int reqHeight) {
+
+        base64String = base64String.replace("data:image/png;base64,","");
+        byte[] decodedString = Base64.decode(base64String, Base64.DEFAULT);
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length, options);
+    }
+
+    private static int calculateInSampleSize(BitmapFactory.Options options,
+                                             int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+}
