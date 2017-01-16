ee72ef507b54ff2951e8660552ad0acfc08f6e78
memuyskens <morganmuyskens@gmail.com>
2017-01-16 03:01:41 -0500

    Fixed the announcement model to support underscored JSON fields.

--- app/src/test/java/com/spartahack/spartahack17/Presenter/AnnouncementPresenterTest.java
+++ app/src/test/java/com/spartahack/spartahack17/Presenter/AnnouncementPresenterTest.java
@@ -3,7 +3,6 @@ package com.spartahack.spartahack17.Presenter;
 import android.util.Log;
 
 import com.spartahack.spartahack17.Model.Announcement;
-import com.spartahack.spartahack17.Retrofit.GSONMock;
 import com.spartahack.spartahack17.View.AnnouncementView;
 
 import org.junit.Test;
@@ -46,40 +45,38 @@ public class AnnouncementPresenterTest extends BaseUnitTest {
 
     @Test public void testCompare() throws Exception {
         Announcement rhsPin = new Announcement();
-        rhsPin.setPinned(true);
+        rhsPin.setPinned(1);
         Announcement lhsPin = new Announcement();
-        lhsPin.setPinned(true);
+        lhsPin.setPinned(1);
         Announcement rhsNoPin = new Announcement();
-        rhsNoPin.setPinned(false);
+        rhsNoPin.setPinned(0);
         Announcement lhsNoPin = new Announcement();
-        lhsNoPin.setPinned(false);
+        lhsNoPin.setPinned(0);
 
         assertEquals(1, presenter.compare(lhsNoPin, rhsPin));
         assertEquals(-1, presenter.compare(lhsPin, rhsNoPin));
 
         // test case where both are not pinned and same time
-        lhsNoPin.setTime("2016-02-28T22:15:00");
-        rhsNoPin.setTime("2016-02-28T22:15:00");
+        lhsNoPin.setCreatedAt("2016-02-28T22:15:00");
+        rhsNoPin.setCreatedAt("2016-02-28T22:15:00");
         assertEquals(0, presenter.compare(lhsNoPin, rhsNoPin));
 
         // both pinned and left first
-        lhsNoPin.setTime("2016-02-28T21:15:00");
+        lhsNoPin.setCreatedAt("2016-02-28T21:15:00");
         assertEquals(-1, presenter.compare(rhsNoPin, lhsNoPin));
 
         // both pinned right first
-        rhsNoPin.setTime("2016-02-28T20:15:00");
+        rhsNoPin.setCreatedAt("2016-02-28T20:15:00");
         assertEquals(1, presenter.compare(rhsNoPin, lhsNoPin));
     }
 
     @Test public void testOnNext() throws Exception {
         // create the announcements
-        GSONMock.Announcements announcements = new GSONMock.Announcements();
-        announcements.announcements = new ArrayList<>();
-        announcements.announcements.add(new Announcement());
-        ArrayList<Announcement> announcementArrayList = announcements.announcements;
+        ArrayList<Announcement> announcements = new ArrayList<>();
+        announcements.add(new Announcement());
 
         presenter.onNext(announcements);
-        verify(view).showAnnouncements(announcementArrayList);
+        verify(view).showAnnouncements(announcements);
     }
 
     @Test public void testOnError() throws Exception {
